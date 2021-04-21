8d32b66725b693fb30329a66c6a07adb19593f54
FozlyRabbi <rabbihasan151537@gmail.com>
2021-04-21 18:07:06 +0600

    WeatherAPP Using Plane Object

---
+++ index.js
@@ -0,0 +1,182 @@
+// store
+const store = {
+  city: "",
+  country: "",
+  defaultCity: "Gazipur",
+  defaultCountry: "BD",
+
+  getLocation() {
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem("city");
+    }
+
+    if (localStorage.getItem("country") === null) {
+      this.country = this.defaultCountry;
+    } else {
+      this.country = localStorage.getItem("country");
+    }
+    return {
+      city: this.city,
+      country: this.country,
+    };
+  },
+  setLocation(city, country) {
+    localStorage.setItem("city", city);
+    localStorage.setItem("country", country);
+  },
+};
+
+//=====================================================================================================//
+
+// Ui
+const ui = {
+  city: document.querySelector("#w-city"),
+  icon: document.querySelector("#w-icon"),
+  temperature: document.querySelector("#w-temp"),
+  Perature: document.querySelector("#w-prea"),
+  Humidity: document.querySelector("#w-humidity"),
+  feel: document.querySelector("#w-feel"),
+
+  print({
+    main_data: { temp, pressure, humidity },
+    overall_data: { main, icon },
+    cityName,
+  }) {
+    const iconUrl = ui.generateIcon(icon);
+
+    this.city.textContent = cityName;
+
+    this.icon.setAttribute("src", iconUrl);
+    // this.icon.textContent = weather.getWeather().overall_data;
+
+    this.temperature.innerHTML = `Temperature: ${temp} <sup>&#8451</sup>`;
+
+    this.Perature.textContent = `Perature(hpa): ${pressure}`;
+
+    this.Humidity.textContent = `Humidity : ${humidity}%`;
+
+    this.feel.textContent = main;
+  },
+
+  generateIcon(icon) {
+    return "https://openweathermap.org/img/w/" + icon + ".png";
+  },
+
+  clearField() {
+    document.querySelector("#country").value = "";
+    document.querySelector("#city").value = "";
+  },
+
+  setMassege(mag) {
+    const para = document.querySelector("#massegeWrapper");
+
+    setTimeout(() => {}, 1000);
+    const div = document.createElement("div");
+    div.id = "message";
+
+    div.className = "alert alert-danger d-flex";
+    div.textContent = mag;
+
+    const icon = document.createElement("span");
+    icon.id = "icon";
+    icon.className = "ml-auto";
+    icon.innerHTML = `<i class="far fa-times-circle" id="close"></i>`;
+
+    para.appendChild(div);
+
+    div.appendChild(icon);
+
+    icon.style.cursor = "pointer";
+
+    //     <div class="alert alert-danger" role="alert">
+    //     This is a danger alert—check it out!
+    // </div>
+  },
+
+  closeMassege() {
+    document
+      .querySelector("#massegeWrapper")
+      .addEventListener("click", function (e) {
+        if ((e.target.id = "icon")) {
+          e.target.parentElement.parentElement.remove();
+        }
+      });
+
+    console.log("hello");
+  },
+};
+
+//=====================================================================================================//
+
+//Weather
+
+const weather = {
+  city: "Gazipur",
+  country: "BD",
+  APPId: "00687502f6fed1a6cce0582a4a77c1ce",
+
+  async getWeather() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.APPId}&units=metric`
+    );
+    const responseData = await response.json();
+
+    return {
+      main_data: responseData.main,
+      cityName: responseData.name,
+
+      overall_data: responseData.weather[0],
+    };
+  },
+
+  setLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  },
+};
+
+// //instenset the new class
+function weatherData() {
+  weather
+    .getWeather()
+    .then((data) => {
+      ui.print(data);
+    })
+    .catch((err) => {
+      ui.setMassege("your city is not found");
+    });
+}
+
+//=====================================================================================================//
+
+// Main
+
+const { city, country } = store.getLocation();
+
+weather.city = city;
+weather.country = country;
+document.addEventListener("DOMContentLoaded", weatherData);
+
+document.querySelector("#form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let country = document.querySelector("#country").value;
+
+  let city = document.querySelector("#city").value;
+
+  if (city === "" || country === "") {
+    ui.setMassege("plz input nessary information");
+
+    document.querySelector("#close").addEventListener("click", function (e) {
+      ui.closeMassege();
+    });
+  } else {
+    store.setLocation(city, country);
+    weather.setLocation(city, country);
+    ui.clearField();
+    weatherData();
+    ui.closeMassege();
+  }
+});
