22eb948cac6f6f1a6355d3639912ebb951e5b2fc
FozlyRabbi <rabbihasan151537@gmail.com>
2021-04-22 21:33:40 +0600

    make more refactoring

--- index.js
+++ index.js
@@ -25,37 +25,99 @@ const store = {
     localStorage.setItem("city", city);
     localStorage.setItem("country", country);
   },
+  printingDataFromLocalStorage() {
+    const { city, country } = this.getLocation();
+
+    weather.city = city;
+    weather.country = country;
+  },
 };
 
 //=====================================================================================================//
 
 // Ui
 const ui = {
-  city: document.querySelector("#w-city"),
-  icon: document.querySelector("#w-icon"),
-  temperature: document.querySelector("#w-temp"),
-  Perature: document.querySelector("#w-prea"),
-  Humidity: document.querySelector("#w-humidity"),
-  feel: document.querySelector("#w-feel"),
+  selectors() {
+    const city = document.querySelector("#w-city");
+    const placeIcon = document.querySelector("#w-icon");
+    const temperature = document.querySelector("#w-temp");
+    const placePerature = document.querySelector("#w-prea");
+    const Humidity = document.querySelector("#w-humidity");
+    const feel = document.querySelector("#w-feel");
+    const MainCountry = document.querySelector("#country");
+    const MainCity = document.querySelector("#city");
+    const para = document.querySelector("#massegeWrapper");
+    const massegeWrapper = document.querySelector("#massegeWrapper");
+    const form = document.querySelector("#form");
+    const closeIcon = document.querySelector("#close");
 
+    return {
+      city,
+      placeIcon,
+      temperature,
+      placePerature,
+      Humidity,
+      feel,
+      para,
+      MainCountry,
+      massegeWrapper,
+      MainCity,
+      form,
+      closeIcon,
+    };
+  },
+  init() {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let country = document.querySelector("#country").value;
+
+      let city = document.querySelector("#city").value;
+
+      if (city === "" || country === "") {
+        ui.setMassege("plz input nessary information");
+
+        closeIcon.addEventListener("click", (e) => {
+          ui.closeMassege();
+        });
+      } else {
+        store.setLocation(city, country);
+        weather.setLocation(city, country);
+        ui.clearField();
+        weather.weatherData();
+        ui.closeMassege();
+      }
+    });
+    document.addEventListener("DOMContentLoaded", weather.weatherData);
+    store.printingDataFromLocalStorage();
+  },
   print({
     main_data: { temp, pressure, humidity },
     overall_data: { main, icon },
     cityName,
   }) {
+    const {
+      city,
+      placeIcon,
+      temperature,
+      placePerature,
+      Humidity,
+      feel,
+    } = this.selectors();
+
     const iconUrl = ui.generateIcon(icon);
 
-    this.city.textContent = cityName;
+    city.textContent = cityName;
 
-    this.icon.setAttribute("src", iconUrl);
+    placeIcon.setAttribute("src", iconUrl);
 
-    this.temperature.innerHTML = `Temperature: ${temp} <sup>&#8451</sup>`;
+    temperature.innerHTML = `Temperature: ${temp} <sup>&#8451</sup>`;
 
-    this.Perature.textContent = `Perature(hpa): ${pressure}`;
+    placePerature.textContent = `Perature(hpa): ${pressure}`;
 
-    this.Humidity.textContent = `Humidity : ${humidity}%`;
+    Humidity.textContent = `Humidity : ${humidity}%`;
 
-    this.feel.textContent = main;
+    feel.textContent = main;
   },
 
   generateIcon(icon) {
@@ -63,12 +125,13 @@ const ui = {
   },
 
   clearField() {
-    document.querySelector("#country").value = "";
-    document.querySelector("#city").value = "";
+    const { MainCountry, MainCity } = this.selectors();
+    MainCountry.value = "";
+    MainCity.value = "";
   },
 
   setMassege(mag) {
-    const para = document.querySelector("#massegeWrapper");
+    const { para } = this.selectors();
 
     setTimeout(() => {}, 1000);
     const div = document.createElement("div");
@@ -90,7 +153,8 @@ const ui = {
   },
 
   closeMassege() {
-    document.querySelector("#massegeWrapper").addEventListener("click", (e) => {
+    const { massegeWrapper } = this.selectors();
+    massegeWrapper.addEventListener("click", (e) => {
       if ((e.target.id = "icon")) {
         e.target.parentElement.parentElement.remove();
       }
@@ -125,52 +189,21 @@ const weather = {
     this.city = city;
     this.country = country;
   },
-};
 
-//instenset the new class
-function weatherData() {
-  weather
-    .getWeather()
-    .then((data) => {
-      ui.print(data);
-    })
-    .catch((err) => {
-      ui.setMassege("your city is not found");
-    });
-}
+  weatherData() {
+    weather
+      .getWeather()
+      .then((data) => {
+        console.log();
+        ui.print(data);
+      })
+      .catch((err) => {
+        ui.setMassege("your city is not found");
+      });
+  },
+};
 
 //=====================================================================================================//
 
 // Main
-
-function printingDataFromLocalStorage() {
-  const { city, country } = store.getLocation();
-
-  weather.city = city;
-  weather.country = country;
-}
-
-printingDataFromLocalStorage();
-document.addEventListener("DOMContentLoaded", weatherData);
-
-document.querySelector("#form").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let country = document.querySelector("#country").value;
-
-  let city = document.querySelector("#city").value;
-
-  if (city === "" || country === "") {
-    ui.setMassege("plz input nessary information");
-
-    document.querySelector("#close").addEventListener("click", (e) => {
-      ui.closeMassege();
-    });
-  } else {
-    store.setLocation(city, country);
-    weather.setLocation(city, country);
-    ui.clearField();
-    weatherData();
-    ui.closeMassege();
-  }
-});
+ui.init();
