35509fbb17d8fe8e629e8bbeb655618f70c2c920
samim <samimfazlu@gmail.com>
2021-06-13 13:11:20 +0600

    Weather application using object

--- index.js
+++ index.js
@@ -1,209 +1,170 @@
-// store
-const store = {
-  city: "",
-  country: "",
-  defaultCity: "Gazipur",
-  defaultCountry: "BD",
+//UI
+//Storage
+const storage = {
+  save(city, country) {
+    localStorage.setItem('city', city)
+    localStorage.setItem('country', country)
+  },
+  getData() {
+    const city = localStorage.getItem('city')
+    const country = localStorage.getItem('country')
+    return { city, country }
+  }
+}
 
-  //geting data from local storage
-  getLocation() {
-    localStorage.getItem("city") === null
-      ? (this.city = this.defaultCity)
-      : (this.city = localStorage.getItem("city"));
+//function with less parameter is more easy to handle
+const http = {
+  city: 'Chandpur',
+  country: 'BD',
+  async getWeatherData() {
+    try {
+      const api_key = 'b6f9d9db9a5d8afcb113312a99248002'
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=metric&appid=${api_key}`
+      )
+      const data = await res.json()
 
-    localStorage.getItem("country") === null
-      ? (this.country = this.defaultCountry)
-      : (this.country = localStorage.getItem("country"));
+      if (data.cod === '404' && data.message) {
+        UI.showMessage(data.message)
+        return
+      }
 
+      UI.paint(data)
+    } catch (err) {
+      UI.showMessage(err.message)
+    }
+  }
+}
+
+const UI = {
+  loadSelectors() {
+    const messageElm = document.querySelector('#messageWrapper')
+    const countryInputElm = document.querySelector('#country')
+    const cityInputElm = document.querySelector('#city')
+    const BtnElm = document.querySelector('#button')
+    const tempElm = document.querySelector('#w-temp')
+    const cityElm = document.querySelector('#w-city')
+    const iconElm = document.querySelector('#w-icon')
+    const pressureElm = document.querySelector('#w-pressure')
+    const feelElm = document.querySelector('#w-feel')
+    const humidityElm = document.querySelector('#w-humidity')
     return {
-      city: this.city,
-      country: this.country,
-    };
+      messageElm,
+      countryInputElm,
+      cityInputElm,
+      BtnElm,
+      tempElm,
+      cityElm,
+      iconElm,
+      pressureElm,
+      feelElm,
+      humidityElm
+    }
   },
-  // seting data to local storage
-  setLocation(city, country) {
-    localStorage.setItem("city", city);
-    localStorage.setItem("country", country);
+  showMessage(msg) {
+    const { messageElm } = this.loadSelectors()
+
+    const elm = `<div id="message" class="alert alert-danger d-flex">
+            ${msg}
+            <span id="icon" class="ml-auto" style="cursor: pointer"
+              ><i class="far fa-times-circle" id="close"></i
+            ></span>
+          </div>`
+    //showing the error message
+    messageElm.insertAdjacentHTML('afterbegin', elm)
+
+    //checking if the error message exist or not
+    if (document.querySelector('#message')) {
+      //hiding the message
+      this.hideMessage()
+    }
   },
-  printingDataFromLocalStorage() {
-    const { city, country } = this.getLocation();
+  hideMessageInst() {
+    const msgInnerElm = document.querySelector('#message')
 
-    weather.city = city;
-    weather.country = country;
+    msgInnerElm.remove()
   },
-};
+  hideMessage() {
+    const msgInnerElm = document.querySelector('#message')
+    setTimeout(() => {
+      msgInnerElm.remove()
+    }, 5000)
+  },
+  paint(weatherData) {
+    const {
+      name,
+      main: { pressure, temp, humidity }
+    } = weatherData
 
-//=====================================================================================================//
+    const { icon, main } = weatherData.weather[0]
+    console.log(weatherData)
 
-// Ui
-const ui = {
-  selectors() {
-    const city = document.querySelector("#w-city");
-    const placeIcon = document.querySelector("#w-icon");
-    const temperature = document.querySelector("#w-temp");
-    const placePerature = document.querySelector("#w-prea");
-    const Humidity = document.querySelector("#w-humidity");
-    const feel = document.querySelector("#w-feel");
-    const MainCountry = document.querySelector("#country");
-    const MainCity = document.querySelector("#city");
-    const para = document.querySelector("#massegeWrapper");
-    const massegeWrapper = document.querySelector("#massegeWrapper");
-    const form = document.querySelector("#form");
-    const closeIcon = document.querySelector("#close");
+    const iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png'
 
-    return {
-      city,
-      placeIcon,
-      temperature,
-      placePerature,
-      Humidity,
-      feel,
-      para,
-      MainCountry,
-      massegeWrapper,
-      MainCity,
-      form,
-      closeIcon,
-    };
+    const {
+      cityElm,
+      tempElm,
+      pressureElm,
+      humidityElm,
+      feelElm,
+      iconElm
+    } = this.loadSelectors()
+    tempElm.textContent = `Temperature:${temp}℃`
+    pressureElm.textContent = `Pressure:${pressure} kpa`
+    humidityElm.textContent = `Humidity:${humidity}`
+    feelElm.textContent = main
+    cityElm.textContent = name
+    //setting up icon
+    iconElm.setAttribute('src', iconUrl)
+  },
+  resetInput() {
+    const { cityInputElm, countryInputElm } = this.loadSelectors()
+    cityInputElm.value = ''
+    countryInputElm.value = ''
   },
   init() {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      let country = document.querySelector("#country").value;
-
-      let city = document.querySelector("#city").value;
-
-      if (city === "" || country === "") {
-        ui.setMassege("plz input nessary information");
-
-        closeIcon.addEventListener("click", (e) => {
-          ui.closeMassege();
-        });
-      } else {
-        store.setLocation(city, country);
-        weather.setLocation(city, country);
-        ui.clearField();
-        weather.weatherData();
-        ui.closeMassege();
-      }
-    });
-    document.addEventListener("DOMContentLoaded", weather.weatherData);
-    store.printingDataFromLocalStorage();
-  },
-  print({
-    main_data: { temp, pressure, humidity },
-    overall_data: { main, icon },
-    cityName,
-  }) {
     const {
-      city,
-      placeIcon,
-      temperature,
-      placePerature,
-      Humidity,
-      feel,
-    } = this.selectors();
+      BtnElm,
+      cityInputElm,
+      countryInputElm,
+      messageElm
+    } = this.loadSelectors()
 
-    const iconUrl = ui.generateIcon(icon);
+    BtnElm.addEventListener('click', e => {
+      //prevent form submission
+      e.preventDefault()
 
-    city.textContent = cityName;
-
-    placeIcon.setAttribute("src", iconUrl);
-
-    temperature.innerHTML = `Temperature: ${temp} <sup>&#8451</sup>`;
-
-    placePerature.textContent = `Perature(hpa): ${pressure}`;
-
-    Humidity.textContent = `Humidity : ${humidity}%`;
-
-    feel.textContent = main;
-  },
-
-  generateIcon(icon) {
-    return "https://openweathermap.org/img/w/" + icon + ".png";
-  },
-
-  clearField() {
-    const { MainCountry, MainCity } = this.selectors();
-    MainCountry.value = "";
-    MainCity.value = "";
-  },
-
-  setMassege(mag) {
-    const { para } = this.selectors();
-
-    setTimeout(() => {}, 1000);
-    const div = document.createElement("div");
-    div.id = "message";
-
-    div.className = "alert alert-danger d-flex";
-    div.textContent = mag;
-
-    const icon = document.createElement("span");
-    icon.id = "icon";
-    icon.className = "ml-auto";
-    icon.innerHTML = `<i class="far fa-times-circle" id="close"></i>`;
-
-    para.appendChild(div);
-
-    div.appendChild(icon);
-
-    icon.style.cursor = "pointer";
-  },
-
-  closeMassege() {
-    const { massegeWrapper } = this.selectors();
-    massegeWrapper.addEventListener("click", (e) => {
-      if ((e.target.id = "icon")) {
-        e.target.parentElement.parentElement.remove();
+      const city = cityInputElm.value
+      const country = countryInputElm.value
+      if (city === '' || country === '') {
+        //show error message
+        this.showMessage('Please fill up the required field')
+      } else {
+        http.city = city
+        http.country
+        http.getWeatherData()
+        this.resetInput()
+        //save city and country to localStorage
+        storage.save(city, country)
       }
-    });
-  },
-};
+    })
 
-//=====================================================================================================//
+    messageElm.addEventListener('click', e => {
+      if (e.target.id === 'close') {
+        this.hideMessageInst()
+      }
+    })
+    //DOM content loaded
+    window.addEventListener('DOMContentLoaded', () => {
+      //getting saved data from localStorage
+      const { city, country } = storage.getData()
 
-//Weather
+      http.city = city
+      http.country = country
 
-const weather = {
-  city: "Gazipur",
-  country: "BD",
-  APPId: "00687502f6fed1a6cce0582a4a77c1ce",
+      http.getWeatherData()
+    })
+  }
+}
 
-  async getWeather() {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.APPId}&units=metric`
-    );
-    const responseData = await response.json();
-
-    return {
-      main_data: responseData.main,
-      cityName: responseData.name,
-
-      overall_data: responseData.weather[0],
-    };
-  },
-
-  setLocation(city, country) {
-    this.city = city;
-    this.country = country;
-  },
-
-  weatherData() {
-    weather
-      .getWeather()
-      .then((data) => {
-        console.log();
-        ui.print(data);
-      })
-      .catch((err) => {
-        ui.setMassege("your city is not found");
-      });
-  },
-};
-
-//=====================================================================================================//
-
-// Main
-ui.init();
+UI.init()
